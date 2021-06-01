26875b528f981be09cd2f8d0780fef6e1695a401
RyanCheungJF <63341400+RyanCheungJF@users.noreply.github.com>
2021-06-01 13:22:09 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,30 +1,80 @@
-//Ryan Cheung Jing Feng Friend), [28.05.21 14:49]
-/*
-const fetch = require("node-fetch");
-
-fetch('https://sg.linkedin.com/in/benleong')
-    .then(function(response) {
-        return response.text(); //Return html tags
-    }).then(function(html) {
-        console.log(html);   //Shows on console
-    }).catch(function(err) {
-        console.warn('Something went wrong.', err);  //Catches errors
-    });
-    */
-
 const paragraphs = document.querySelectorAll('p');
-alert(paragraphs[0].childNodes[0].nodeValue)
-var counter = 0;
-window.addEventListener("keypress",
-    function(e) {
-        if (e.key.charCodeAt(0) === 49) {
-            let engine = new SpeechSynthesisUtterance(paragraphs[counter].childNodes[0].nodeValue);
-            engine.volume = 1;
-            engine.rate = 1;
-            window.speechSynthesis.cancel();
-            window.speechSynthesis.speak(engine);
-            console.log("It is supposed to work");
-            counter+=1
-        }
+var para_counter = 0;
+const h1 = document.querySelectorAll('h1');
+var h1_counter = 0;
+const h2 = document.querySelectorAll('h2');
+var h2_counter = 0;
+const h3 = document.querySelectorAll('h3');
+const h4 = document.querySelectorAll('h4');
+const h5 = document.querySelectorAll('h5');
+const h6 = document.querySelectorAll('h6');
+var delta = 500;
 
-    });
+
+window.addEventListener("keypress",
+    function(event) {
+        var pressed = false;        
+        if (event.key.charCodeAt(0) === 49) {
+            var curr_press = new Date();
+            if (pressed) {
+                para_counter === 0 ? para_counter = paragraphs.length : para_counter = para_counter--;
+            } 
+            pressed = !pressed;
+            if (para_counter > paragraphs.length) {
+                para_counter = 0;
+            }
+            let engine = new SpeechSynthesisUtterance(paragraphs[para_counter].childNodes[0].nodeValue);
+            engine.rate = 1.2;
+            window.speechSynthesis.speak(engine);
+            para_counter++;
+            curr_press = 0;
+            window.setTimeout(function() {pressed = false;}, 500);
+        }
+    }
+);
+
+new Promise((resolve, reject) => {
+    if (h1.length !== 0) {
+        resolve("Reading h1 tags!");
+    } else {
+        reject("No h1 tags available!");
+    }
+}).then((msg) => {
+    window.addEventListener("keypress",
+        function(event) {
+            if (event.key.charCodeAt(0) === 50) {
+                if (h1_counter > h1.length) {
+                    h1_counter = 0;
+                }
+                let engine = new SpeechSynthesisUtterance((h1)[h1_counter].innerText);
+                window.speechSynthesis.speak(engine);
+                h1_counter++;
+            }
+        }
+    )
+}).catch((msg) => {
+    console.log(msg);
+});
+
+new Promise((resolve, reject) => {
+    if (h2.length !== 0) {
+        resolve("Reading h2 tags!");
+    } else {
+        reject("No h2 tags available!");
+    }
+}).then((msg) => {
+    window.addEventListener("keypress",
+        function(event) {
+            if (event.key.charCodeAt(0) === 51) {
+                if (h2_counter > h2.length) {
+                    h2_counter = 0;
+                }
+                let engine = new SpeechSynthesisUtterance((h2)[h2_counter].innerText);
+                window.speechSynthesis.speak(engine);
+                h2_counter++;
+            }
+        }
+    )
+}).catch((msg) => {
+    console.log(msg);
+});
