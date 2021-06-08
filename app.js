6ebf628304fd4bce51d1e1c10841d8a7e4c9b33e
RyanCheungJF <63341400+RyanCheungJF@users.noreply.github.com>
2021-06-08 11:30:37 +0800

    Update app.js

--- app.js
+++ app.js
@@ -11,11 +11,13 @@ var headers_counter = 0;
 const links = document.querySelectorAll('a');
 var links_counter = 0;
 var searched = false;
+var last_link = null;
 //****************************************************************** 
 
 
 // Hyperlink Handling
 var hyperlinks = [];
+var linkTitles = [];
 // Interestingly, this gives an error if done with a simple for loop statement, 
 // reference: https://stackoverflow.com/questions/40835675/why-does-chrome-throw-getattribute-is-not-a-function-error-inside-a-for-loop
 for (var link = 0; link < links.length; link++)  {      
@@ -23,21 +25,44 @@ for (var link = 0; link < links.length; link++)  {
     // Certain links will not return the full link path, hence we manually concat it to the current webpage we are on. 
     // eg. returns /section/politics after filtering but link should be https://www.nytimes.com/section/politics
     if (hyperlink != null && hyperlink.substring(0, 4) != "http") {
-        hyperlink = window.location.href.concat(hyperlink.substring(1, hyperlink.length));
+        if (hyperlink.substring(0, 1) == "/") {
+            hyperlink = window.location.href.concat(hyperlink.substring(1, hyperlink.length));
+        } else {
+            hyperlink = window.location.href.concat(hyperlink.substring(0, hyperlink.length));
+        }
+    }
+    let linkName = links[link].innerText;
+    // Certain links do not have a name associated, so we sieve through the url link to obtain one.
+    // Has potential for machine learning techniques.
+    if (linkName == "") {
+        var lastslash = hyperlink.lastIndexOf("/");
+        var str = hyperlink.substring(lastslash + 1, hyperlink.length);
+        if (str.indexOf(".html") != -1) {
+            str = str.substring(0, str.indexOf(".html"));
+        }
+        linkName = str.replace("-", " ");
     }
     hyperlinks[link] = hyperlink;
+    linkTitles[link] = linkName;
+    console.log(link);
+    console.log(hyperlink);
+    console.log(linkName);
 }
 
 // Additional function to filter through the hyperlinks
 var filterlinks = [];
+var filtertitles = [];
 function findLink(str) {
     filterlinks = [];
+    filtertitles = [];
     for (i in hyperlinks) {
         if (hyperlinks[i].includes(str)) {
             filterlinks.push(hyperlinks[i]);
+            filtertitles.push(linkTitles[i]);
         }
     }
 }
+
 
 // This event listener will await a prompt from the user to search for. Entering nothing will return all hyperlinks.
 window.addEventListener("keypress",
@@ -71,8 +96,36 @@ window.addEventListener("keypress",
                 if (links_counter === filterlinks.length) {
                     links_counter = 0;
                 }
-                engine.speak(new SpeechSynthesisUtterance(filterlinks[links_counter]));
+                engine.speak(new SpeechSynthesisUtterance(filtertitles[links_counter]));
+                links_counter++;
             }
+        }
+    }
+);
+
+// Used to visit a hyperlink after choosing.
+window.addEventListener("keypress",
+    function(event) {
+        if (event.key.charCodeAt(0) === 48) {
+            let engine = window.speechSynthesis;
+            engine.rate = speak_rate;
+            if (last_link == null) {
+                engine.speak(new SpeechSynthesisUtterance("Please search for a keyword by hitting 3!"));
+            } else {
+                engine.speak(new SpeechSynthesisUtterance("Redirecting you to " + last_link[1]));
+                window.location.href = last_link[0];
+            }
+        }
+    }
+);
+
+// Reporting ads. ("r" key)
+window.addEventListener("keypress",
+    function(event) {
+        if (event.key.charCodeAt(0) === 114) {
+            let engine = window.speechSynthesis;
+            engine.rate = speak_rate;
+            console.log("kachow");
         }
     }
 );
