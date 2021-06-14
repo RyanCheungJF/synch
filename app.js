b1c09b65614fffcbfd140fc58e913f212149f180
RyanCheungJF <63341400+RyanCheungJF@users.noreply.github.com>
2021-06-14 12:06:17 +0800

    Update app.js
    Added highlighting functionality

--- app.js
+++ app.js
@@ -1,29 +1,28 @@
-texttoSpeech// Global speed, future feature to customize voice of reader and speed.
-var speak_rate = 1.1;
-//******************************************************************
+// Global speed, future feature to customize voice of reader and speed.
+let engine = window.speechSynthesis;
+engine.rate = 1.4;
+//****************************************************************** 
 const paragraphs = document.querySelectorAll('p');
 var para_counter = -1;
 var para_pressed = false;
-//******************************************************************
+var para_lastHTML = paragraphs[paragraphs.length - 1].innerHTML;
+//****************************************************************** 
 const headers = document.querySelectorAll('h1, h2, h3, h4, h5, h6');
-var headers_counter = 0;
-//******************************************************************
+var headers_counter = -1;
+var headers_pressed = false;
+var headers_lastHTML = headers[headers.length - 1].innerHTML;
+//****************************************************************** 
 const links = document.querySelectorAll('a');
 var links_counter = 0;
 var searched = false;
 var last_link = null;
-//******************************************************************
-
+//****************************************************************** 
 
 // Hyperlink Handling
 var hyperlinks = [];
 var linkTitles = [];
-// Interestingly, this gives an error if done with a simple for loop statement,
-// reference: https://stackoverflow.com/questions/40835675/why-does-chrome-throw-getattribute-is-not-a-function-error-inside-a-for-loop
-for (var link = 0; link < links.length; link++)  {
+for (var link = 0; link < links.length; link++)  {      
     let hyperlink = links[link].getAttribute("href");
-    // Certain links will not return the full link path, hence we manually concat it to the current webpage we are on.
-    // eg. returns /section/politics after filtering but link should be https://www.nytimes.com/section/politics
     if (hyperlink != null && hyperlink.substring(0, 4) != "http") {
         if (hyperlink.substring(0, 1) == "/") {
             hyperlink = window.location.href.concat(hyperlink.substring(1, hyperlink.length));
@@ -32,10 +31,7 @@ for (var link = 0; link < links.length; link++)  {
         }
     }
     let linkName = links[link].innerText;
-    // Certain links do not have a name associated, so we sieve through the url link to obtain one.
-    // Has potential for machine learning techniques.
     if (linkName == "") {
-        //Pops up an error because google returns a null for the hyperlink element. Need to find how to query google searches
         var lastslash = hyperlink.lastIndexOf("/");
         var str = hyperlink.substring(lastslash + 1, hyperlink.length);
         if (str.indexOf(".html") != -1) {
@@ -45,9 +41,9 @@ for (var link = 0; link < links.length; link++)  {
     }
     hyperlinks[link] = hyperlink;
     linkTitles[link] = linkName;
-    console.log(link);
-    console.log(hyperlink);
-    console.log(linkName);
+    //console.log(link);
+    //console.log(hyperlink);
+    //console.log(linkName);
 }
 
 // Additional function to filter through the hyperlinks
@@ -57,59 +53,44 @@ function findLink(str) {
     filterlinks = [];
     filtertitles = [];
     for (i in hyperlinks) {
-        if (hyperlinks[i].toLowerCase().includes(str.toLowerCase())) { //Lower case to make case insensitive case
+        if (hyperlinks[i].includes(str)) {
             filterlinks.push(hyperlinks[i]);
             filtertitles.push(linkTitles[i]);
         }
-
-
-
-
     }
 }
-function texttoSpeech(text){   //Just call this everytime u want a speech
-  let engine = window.speechSynthesis;
-  engine.cancel();
-  engine.speak(new SpeechSynthesisUtterance(text));
-
-
-};
-
 
 // This event listener will await a prompt from the user to search for. Entering nothing will return all hyperlinks.
 window.addEventListener("keypress",
     function(event) {
         if (event.key.charCodeAt(0) === 51) {
             searched = true;
-            let engine = window.speechSynthesis;
-            engine.rate = speak_rate;
-
-            texttoSpeech("Please type in the keyword and hit enter to search among hyperlinks!");
+            engine.cancel();
+            engine.speak(new SpeechSynthesisUtterance("Please type in the keyword and hit enter to search among hyperlinks!"));
             var search = window.prompt("Search keyword for hyperlinks: ");
             findLink(search);
-            texttoSpeech("Filtering for keyword " + search);
+            engine.speak(new SpeechSynthesisUtterance("Filtering for keyword " + search));
             if (filterlinks.length === 0) {
-                texttoSpeech("Sorry, there is no link with this keyword " + search);
+                engine.speak(new SpeechSynthesisUtterance("Sorry, there is no link with this keyword " + search));
             } else {
-                texttoSpeech("Done! Press 4 to alternate between the search results!");
+                engine.speak(new SpeechSynthesisUtterance("Done! Press 4 to alternate between the search results!"));
             }
         }
     }
-);
+); 
 
 // Used to alternate through the hyperlinks, similar to paragraphs.
 window.addEventListener("keypress",
     function(event) {
         if (event.key.charCodeAt(0) === 52) {
-            let engine = window.speechSynthesis;
-            engine.rate = speak_rate;
             if (!searched) {
-                texttoSpeech("Please search for a keyword by hitting 3!");
+                engine.speak(new SpeechSynthesisUtterance("Please search for a keyword by hitting 3!"));
             } else {
                 if (links_counter === filterlinks.length) {
                     links_counter = 0;
                 }
-                texttoSpeech(filtertitles[links_counter]);
+                last_link = [filterlinks[links_counter], filtertitles[links_counter]];
+                engine.speak(new SpeechSynthesisUtterance(filtertitles[links_counter]));
                 links_counter++;
             }
         }
@@ -120,51 +101,38 @@ window.addEventListener("keypress",
 window.addEventListener("keypress",
     function(event) {
         if (event.key.charCodeAt(0) === 48) {
-            let engine = window.speechSynthesis;
-            engine.rate = speak_rate;
             if (last_link == null) {
-                texttoSpeech("Please search for a keyword by hitting 3!");
+                engine.speak(new SpeechSynthesisUtterance("Please search for a keyword by hitting 3!"));
             } else {
-                texttoSpeech("Redirecting you to " + last_link[1]);
+                engine.speak(new SpeechSynthesisUtterance("Redirecting you to " + last_link[1]));
                 window.location.href = last_link[0];
             }
         }
     }
 );
 
-// Reporting ads. ("r" key)
-window.addEventListener("keypress",
-    function(event) {
-        if (event.key.charCodeAt(0) === 114) {
-            let engine = window.speechSynthesis;
-            engine.rate = speak_rate;
-            console.log("kachow");
-        }
-    }
-);
-
 // Paragraphs
-    // **************************************************************************************************
-    // Currently has the functionality where if double pressed within 500ms, will read the previous para.
-    // Hence, there is back and forward navigation ability.
-    // **************************************************************************************************
-var delta = 500;
 window.addEventListener("keypress",
-    function(event) {
+    function(event) {       
         if (event.key.charCodeAt(0) === 49) {
-            let engine = window.speechSynthesis;
-            engine.rate = speak_rate;
+            resetHighlights();
             if (para_pressed) {
-                para_counter === 0 ? para_counter = paragraphs.length : para_counter = para_counter - 2;
+                para_counter = para_counter - 2;
                 para_pressed = !para_pressed;
             } else {
                 para_counter++;
                 para_pressed = !para_pressed;
-                window.setTimeout(function() {para_pressed = false;}, 500);
+                window.setTimeout(function() { para_pressed = false; }, 500);
             }
-
-            texttoSpeech(paragraphs[para_counter].childNodes[0].nodeValue);
-
+            para_lastHTML = paragraphs[index(para_counter, paragraphs.length)].innerHTML;
+            engine.cancel();
+            engine.speak(
+                new SpeechSynthesisUtterance(
+                    cleanupText(paragraphs[index(para_counter, paragraphs.length)]
+                        .childNodes[0].nodeValue, paragraphs, para_counter)));
+            window.setTimeout(function() { 
+                paragraphs[index(para_counter, paragraphs.length)].innerHTML = "<mark>" + para_lastHTML + "</mark>"; 
+            }, 500);
         }
     }
 );
@@ -173,42 +141,47 @@ window.addEventListener("keypress",
 window.addEventListener("keypress",
     function(event) {
         if (event.key.charCodeAt(0) === 50) {
-            if (headers_counter > headers.length) {
-                headers_counter = 0;
+            resetHighlights();
+            if (headers_pressed) {
+                headers_counter = headers_counter - 2;
+                headers_pressed = !headers_pressed;
+            } else {
+                headers_counter++;
+                headers_pressed = !headers_pressed;
+                window.setTimeout(function() { headers_pressed = false; }, 500);
             }
-            //let engine = new SpeechSynthesisUtterance(headers[headers_counter].childNodes[0].nodeValue);
-            //engine.rate = speak_rate;
-            //window.speechSynthesis.speak(engine);
-            texttoSpeech(headers[headers_counter].childNodes[0].nodeValue);
-            headers_counter++;
+            headers_lastHTML = headers[index(headers_counter, index.length)].innerHTML;
+            engine.cancel();
+            engine.speak(new SpeechSynthesisUtterance(
+                cleanupText(headers[index(headers_counter, headers.length)]
+                    .childNodes[0].nodeValue, headers, headers_counter)));
+            window.setTimeout(function() { 
+                headers[index(headers_counter, headers.length)].innerHTML = "<mark>" + headers_lastHTML + "</mark>"; 
+            }, 500);
         }
-    });
-
-
-
-
-// Test/ Debug Code
-/*
-new Promise((resolve, reject) => {
-    if (h2.length !== 0) {
-        resolve("Reading h2 tags!");
-    } else {
-        reject("No h2 tags available!");
     }
-}).then((msg) => {
-    window.addEventListener("keypress",
-        function(event) {
-            if (event.key.charCodeAt(0) === 51) {
-                if (h2_counter > h2.length) {
-                    h2_counter = 0;
-                }
-                let engine = new SpeechSynthesisUtterance((h2)[h2_counter].innerText);
-                window.speechSynthesis.speak(engine);
-                h2_counter++;
-            }
+);
+
+// Math function to deal with indexing as we use modulo function
+function index(num, len) {
+    return num % len < 0 ? len + num : num % len;
+}
+
+// Used to reset the highlighting on words
+function resetHighlights() {
+    paragraphs[index(para_counter, paragraphs.length)].innerHTML = para_lastHTML;
+    headers[index(headers_counter, headers.length)].innerHTML = headers_lastHTML;
+}
+
+// Certain strings are null as they are wrapped in other tags, this just tries to parse the content out
+function cleanupText(str, arr, count) {
+    if (str == null) {
+        str = arr[index(count % arr.length, arr.length)].innerHTML;
+        while(str.indexOf('<') != -1 && str.indexOf('>') != -1) {
+            var left = str.indexOf('<');
+            var right = str.indexOf('>');
+            str = str.substring(0, left) + str.substring(right + 1, str.length);
         }
-    )
-}).catch((msg) => {
-    console.log(msg);
-});
-*/
+    }
+    return str;
+}
