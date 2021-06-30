60cc91d9242e5b03dba201fd030a6fb5eb6cee1a
Niran-Chan <niranshivu@gmail.com>
2021-06-30 14:42:50 +0800

    Converted easylist to json format and modified manifest.json a bit

--- adblock.js
+++ adblock.js
@@ -1,1 +1,16 @@
-console.log("Hello World!")
+
+/*
+var easy_list = e.
+var blocked_sites = JSON.parse(easy_list);
+*/
+
+var blocked_sites = ["*googleadservices.com/*",
+,"*tigerbrokers.com.sg/*"];
+
+chrome.WebRequest.onBeforeRequest.addListener(function(content){
+  console.log("BLOCKING",content.url)
+  return {cancel: true}
+},
+{urls:blocked_sites},
+["blocking"]
+)
---
+++ convert.py
@@ -0,0 +1,18 @@
+
+
+f = open("easylist.rst","r")
+f_out = open("easy_list.json","w")
+arr = []
+
+while 1:
+    try:
+        content = f.readline()
+        if content == "":
+            break
+        arr.append('\"' + "".join(content) + '\"')
+    except Exception:
+        pass
+
+f_out.write(("{"+','.join(arr) + "}"))
+f.close()
+f_out.close()
