3cb2438311fe1be19f17636b5d655bd3131358af
Niran-Chan <niranshivu@gmail.com>
2021-07-05 15:02:03 +0800

    Changed to our Github Page

--- popup.js
+++ popup.js
@@ -7,8 +7,8 @@ document.querySelector('#options').addEventListener('click',function() {
 });
 
 document.querySelector('#ourGithub').addEventListener('click', function () {
-    
-    window.open('https://github.com/Niran-Chan'); //Load our Github!
+
+    window.open('https://github.com/RyanCheungJF/Synch'); //Load our Github!
 
 
 });
