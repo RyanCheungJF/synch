3c211083856b33a7eec066af7ec0f4cfe6da564d
Niran-Chan <niranshivu@gmail.com>
2021-07-05 15:23:12 +0800

    No need for 2 service workers as extension only works with one.

--- background.js
+++ background.js
@@ -1,13 +1,36 @@
 console.log("background.js working");
-/*
+
+//ContextMenu
+
+chrome.contextMenus.create({
+    "id":"speakwords",
+    "title":"Speak Words",
+    "type": "normal",
+    "contexts":["selection"]
+},function() {
+    if (chrome.extension.lastError) {
+      console.log("Got expected error: " + chrome.extension.lastError.message);
+    }
+});
+
+chrome.contextMenus.onClicked.addListener(function(clickData){
+  if(clickData.menuItemId == "speakwords" && clickData.selectionText){
+    let engine = chrome.tts;
+    engine.speak(clickData.selectionText);
+
+    console.log("Speaking now");
+
+  }
+});
+
+
+
+
+
 chrome.commands.onCommand.addListener(function(command){
 
-  if (chrome.runtime.openOptionsPage) {
-      chrome.runtime.openOptionsPage();
-  }
-  else {
-      chrome.runtime.getURL('options.html');
-  }
+  if(command == headers):
+
 
 
 
