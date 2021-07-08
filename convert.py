add414512690329a21bd3a4a23c49e4bd04f6afa
Niran-Chan <niranshivu@gmail.com>
2021-07-08 17:11:57 +0800

    Added a legitimate rules.json as easy_list_3.json. Still having issues with resource types

--- convert.py
+++ convert.py
@@ -1,16 +1,33 @@
 f = open("easylist.rst","r")
-f_out = open("easy_list.json","w")
+f_out = open("easy_list_3.json","w")
 arr = []
+id = 1
 
 while 1:
     try:
-        content = f.readline()
-        if content == "":
+        data = f.readline()
+        
+        if data == "":
             break
-        arr.append('\"' + "".join(content) + '\"')
+        if "\\" in data:
+            data = data.replace("\\","\\\\")
+        if "\"" in data:
+            data = data.replace("\"","\\\"")
+        content = "{"+"\"id\""+":%s,"%(id) + "\"priority\"" + ": 1,"+"\"action\""+": { \"type\": \"block\" }," + "\"condition\"" + ": {\"urlFilter\":" + "\"{}\"".format(data) + ", \"resourceTypes\": [\"sub_frame\",\"image\",\"script\",\"main_frame\"]}}"
+
+        arr.append("".join(content))
+
+        id +=1
     except Exception:
         pass
 
-f_out.write(("{"+','.join(arr) + "}"))
+f_out.write(("["+','.join(arr) + "]"))
 f.close()
 f_out.close()
+
+"""{
+"id": 2,
+"priority": 1,
+"action": { "type": "block" },
+"condition": {"urlFilter":"*googlesyndication.com/*", "resourceTypes": ["main_frame"] }
+}"""
